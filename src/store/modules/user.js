ae327323f6e484cbabeb6409124eb472c95cc2a0
ZPPeter <edmic@163.com>
2019-07-04 00:11:19 +0800

    测试 adp.dapper  查询

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -19,11 +19,20 @@ class UserModule extends ListModule {
         this.actions = {
             async getAll(context, payload) {
                 context.state.loading = true;
+
                 let reponse = await Ajax.get('/api/services/app/User/GetAll', { params: payload.data });
                 context.state.loading = false;
                 let page = reponse.data.result;
                 context.state.totalCount = page.totalCount;
                 context.state.list = page.items;
+                
+                /* abp.dapper 版本需要完善 分页等
+                  let reponse = await Ajax.get('/api/services/app/Person/GetDapperPersons',{params:payload.data});
+                  context.state.loading = false;
+                  context.state.totalCount = 1;//page.totalCount;
+                  context.state.list = reponse.data.result;
+                */
+
             },
             async create(context, payload) {
                 await Ajax.post('/api/services/app/User/Create', payload.data);
